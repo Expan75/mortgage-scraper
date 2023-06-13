b55cd26653037e7f8da09cd632afc5fe23b23a87
hakan <erik.hakansson96@gmail.com>
2023-06-13 12:10:55 +0200

    e2e tests for each provider

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -45,3 +45,57 @@ def test_should_use_proxy_if_available(entrypoint: str, project_dir: Path):
         assert result.returncode == 0, "should exit without error code"
         assert len(os.listdir(data_dir)) > len(files), "test run did not store .csv"
 
+
+def test_should_scrape_hypoteket(entrypoint: str, project_dir: Path):
+    data_dir = os.path.join(project_dir, "data")
+    files = os.listdir(data_dir)
+    result = subprocess.run([
+        "python3", entrypoint, 
+        "--target", "hypoteket", 
+        "--store", "csv", 
+        "--limit", "1"
+    ])
+    assert result.returncode == 0, "should exit without error code"
+    assert len(os.listdir(data_dir)) > len(files), "test run did not yield stored csv"
+
+
+
+def test_should_scrape_ica(entrypoint: str, project_dir: Path):
+    data_dir = os.path.join(project_dir, "data")
+    files = os.listdir(data_dir)
+    result = subprocess.run([
+        "python3", entrypoint, 
+        "--target", "ica", 
+        "--store", "csv", 
+        "--limit", "1"
+    ])
+    assert result.returncode == 0, "should exit without error code"
+    assert len(os.listdir(data_dir)) > len(files), "ica run did not yield stored csv"
+
+
+def test_should_scrape_sbab(entrypoint: str, project_dir: Path):
+    data_dir = os.path.join(project_dir, "data")
+    files = os.listdir(data_dir)
+    result = subprocess.run([
+        "python3", entrypoint, 
+        "--target", "sbab", 
+        "--store", "csv", 
+        "--limit", "1"
+    ])
+    assert result.returncode == 0, "should exit without error code"
+    assert len(os.listdir(data_dir)) > len(files), "test run did not yield stored csv"
+
+
+
+def test_should_scrape_skandia(entrypoint: str, project_dir: Path):
+    data_dir = os.path.join(project_dir, "data")
+    files = os.listdir(data_dir)
+    result = subprocess.run([
+        "python3", entrypoint, 
+        "--target", "skandia", 
+        "--store", "csv", 
+        "--limit", "1"
+    ])
+    assert result.returncode == 0, "should exit without error code"
+    assert len(os.listdir(data_dir)) > len(files), "test run did not yield stored csv"
+
