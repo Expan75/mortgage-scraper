90ad9e65cca6eaa206a63c97fbe57f90695da173
hakan <erik.hakansson96@gmail.com>
2023-07-03 18:00:06 +0200

    add loaded user agents

--- mortgage_scraper/scraper_config.py
+++ mortgage_scraper/scraper_config.py
@@ -1,17 +1,40 @@
-from typing import Optional
+from typing import Optional, Union, List, Dict
 from pydantic.dataclasses import dataclass
 
 
 @dataclass
 class ScraperConfig:
-    # https://someproxy.com
-    proxy: str = ""
-
-    # caps the number of urls we'd like to scrape
-    max_urls: Optional[int] = None
-
-    # send off urls in random order
-    permutate: bool = False
-
-    # misc
+    # more extensive logging etc.
     debug: bool = False
+
+    # sleep between requests
+    delay: Optional[Union[int, float]] = None
+
+    # intepretad as requests/second
+    rate_limit: Optional[int] = None
+
+    # cap urls, useful for debugging
+    urls_limit: Optional[int] = None
+
+    # send generated urls in a seeded randomised order
+    randomise_url_order: bool = False
+    randomise_url_seed: int = 42
+
+    # switches attached user agent in headers with provided List
+    rotate_user_agent: bool = False
+    user_agents_filepath: str = "../agents.txt"
+
+    # route requests via proxy
+    proxies: List[str] = []
+
+    @property
+    def proxies_by_protocol(self) -> Dict[str, List[str]]:
+        proxies: Dict[str, List[str]] = {"http": [], "https": []}
+        for proxy in self.proxies:
+            protocol = "https" if "https" in proxy else "http"
+            proxies[protocol].append(proxy)
+        return proxies
+
+    @property
+    def proxy(self) -> bool:
+        return len(self.proxies) > 0
