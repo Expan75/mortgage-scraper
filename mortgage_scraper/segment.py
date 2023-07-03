bc0012b0d0d92dcfdd0d81573b9907fa68eb17dd
hakan <erik.hakansson96@gmail.com>
2023-07-03 15:04:16 +0200

    signifantly cut down segment bins ranges queried

--- mortgage_scraper/segment.py
+++ mortgage_scraper/segment.py
@@ -17,7 +17,7 @@ class MortgageMarketSegment:
         return self.loan_amount / self.asset_value
 
     @staticmethod
-    def calculate_asset_value(ltv: float, vol: float) -> float:
+    def infer_asset_value(ltv: float, vol: float) -> float:
         return 1 / (ltv / vol)
 
 
@@ -30,25 +30,31 @@ def generate_segments(period: Optional[str] = None) -> List[MortgageMarketSegmen
 
     ltv = loan_amount / asset_value <=> (1 / (ltv / loan_amount)) = asset_value
 
-    This corresponds to a 2-dimensional market segment
+    This corresponds to a 2-dimensional market segment. Cardinality of cartesian
+    ltv_bins x loan_amount_bins correponds to the number of urls to be sent,
+    forcing us to select bins modestly; bins below are selected to keep the number
+    of unique segmnets below 1 million.
     """
-    loan_amount_bins = np.arange(50_000, 10_000_000, 50_000)
-    ltv_bins = np.arange(0.005, 1, 0.005)
+
+    loan_amount_bins = [
+        *np.arange(50_000, 2_000_000, 50_000),
+        *np.arange(2_000_000, 5_000_000, 250_000),
+        *np.arange(5_000_000, 10_000_000, 500_000),
+    ]
+    ltv_bins = np.arange(0.5, 1, 0.01)
 
     # infer asset values based off of this
-    # for some reason numpy causes underflow, so we rely on python builtin
-
     asset_value_bins = [
-        MortgageMarketSegment.calculate_asset_value(ltv, vol)
+        MortgageMarketSegment.infer_asset_value(ltv, vol)
         for (ltv, vol) in itertools.product(loan_amount_bins, ltv_bins)
     ]
 
-    segments = [
-        MortgageMarketSegment(asset_value, loan_amount, period)
-        for asset_value, loan_amount in itertools.product(
-            asset_value_bins, loan_amount_bins
-        )
-    ]
+    segments = []
+
+    for loan_amount in loan_amount_bins:
+        for asset_value in asset_value_bins:
+            segment = MortgageMarketSegment(asset_value, loan_amount)
+            segments.append(segment)
 
     return segments
 
