27dd7c2ce77f12745860cf0bdf1d349c72bcd24e
hakan <erik.hakansson96@gmail.com>
2023-07-03 15:07:23 +0200

    test for segment

---
+++ tests/test_segment.py
@@ -0,0 +1,23 @@
+from mortgage_scraper.segment import generate_segments, MortgageMarketSegment
+
+
+def test_should_generate_segments():
+    unique_ltvs = set()
+    unique_vols = set()
+    unique_asset_values = set()
+
+    segments = generate_segments()
+
+    assert len(segments) != 0
+    for segment in segments:
+        unique_ltvs.add(segment.ltv)
+        unique_vols.add(segment.loan_amount)
+        unique_asset_values.add(segment.asset_value)
+
+        correct_ltv = segment.ltv == segment.loan_amount / segment.asset_value
+        assert correct_ltv, "ltv ratio not sane"
+
+    assert len(unique_ltvs) > 1, "should have plenty of segments"
+    assert len(unique_vols) > 1, "should have plenty of segments"
+    assert len(unique_asset_values) > 1, "should have plenty of segments"
+    assert len(segments) < 500_000, "should not have an insane amount of segments"
