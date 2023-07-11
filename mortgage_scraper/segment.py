fad240ff31f54e8e4a3bbc5961fec9c0d8b4cf44
hakan <erik.hakansson96@gmail.com>
2023-07-11 15:39:10 +0200

    add arg for loan volume bins

--- mortgage_scraper/segment.py
+++ mortgage_scraper/segment.py
@@ -17,7 +17,18 @@ class MortgageMarketSegment:
         return self.loan_amount / self.asset_value
 
 
-def generate_segments(period: Optional[str] = None) -> List[MortgageMarketSegment]:
+DEFAULT_LOAN_VOLUME_BINS = [
+    *np.arange(50_000, 2_000_000, 50_000).tolist(),
+    *np.arange(2_000_000, 5_000_000, 100_000).tolist(),
+    *np.arange(5_000_000, 10_000_000, 250_000).tolist(),
+]
+
+
+def generate_segments(
+    ltv_granularity: Optional[float] = 0.01,
+    period: Optional[str] = None,
+    loan_volume_bins: List[int] = DEFAULT_LOAN_VOLUME_BINS,
+) -> List[MortgageMarketSegment]:
     """
     Determines the bins to be used for query formatting
 
@@ -27,26 +38,25 @@ def generate_segments(period: Optional[str] = None) -> List[MortgageMarketSegmen
     ltv = loan / asset <=> asset = loan/ltv
 
     This corresponds to a 2-dimensional market segment. Cardinality of cartesian
-    ltv_bins x loan_amount_bins x (unique maturity periods) correponds to the number 
+    ltv_bins x loan_amount_bins x (unique maturity periods) correponds to the number
     of urls to be sent, forcing us to select bins modestlybins
-    
+
     Bins below are selected to keep the number of segments and urls below 1 million.
+
+    LTV:s are generated on [0.5,1] evenly with the provided decimal granuality.
+
+
     """
 
-    loan_amount_bins = [
-        *np.arange(50_000, 2_000_000, 50_000).tolist(),
-        *np.arange(2_000_000, 5_000_000, 100_000).tolist(),
-        *np.arange(5_000_000, 10_000_000, 250_000).tolist(),
-    ]
-    ltv_bins = np.arange(0.5, 1.0, 0.01).tolist()
+    ltv_bins = np.arange(0.5, 1.0, ltv_granularity).tolist()
 
     # infer asset values based off of this
     asset_value_bins = [
-        vol / ltv for (ltv, vol) in itertools.product(ltv_bins, loan_amount_bins)
+        vol / ltv for (ltv, vol) in itertools.product(ltv_bins, loan_volume_bins)
     ]
 
     segments = []
-    for loan_amount in loan_amount_bins:
+    for loan_amount in loan_volume_bins:
         for asset_value in asset_value_bins:
             segment = MortgageMarketSegment(asset_value, loan_amount, period)
             segments.append(segment)
